a1a69bf97795c9360a0dc2c9329e918135c127a3
ChaosmicDoge <marianodomre@gmail.com>
2020-03-04 19:34:52 +0100

    Corrected used fonts, remade the quest system. Problems triggering it and resizing the quest elements.

---
+++ HexClicker/Assets/Quests/Quest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace HexClicker.Quests
+{
+    [CreateAssetMenu(fileName = "Quest", menuName = "Quest")]
+    public class Quest : ScriptableObject // This quest script is a place holder for a full fledged out quest system
+    {
+        public enum Status
+        {
+            Requested,
+            Rejected,
+            Active,
+            Completed,
+            Failed
+        }
+
+        public enum Type
+        {
+            Gathering,
+            Fetching,
+            Fighting,
+            Building,
+            Growing,
+            Boss
+        }
+
+        [Serializable]
+        public struct Goal
+        {
+            public string body;
+            public Status status;
+            public Type type;
+
+            public bool hasTimer;
+            public Timer timer;
+            
+            public bool hasCounter;
+            public Counter counter;
+
+            public bool hasLocation;
+            public Vector3 location;
+            
+        }
+
+        public int id;
+        public string body;
+        public Status status;
+        public Goal[] goals;
+        
+        
+        public string reward; //This string is currently a place holder for an eventual reward system
+
+        public void FailedQuest()
+        {
+            
+            UI.Notifications.NotificationSystem.Instance.Post("Quest Failed", "You've failed the following quest: " + name);
+            status = Status.Failed;
+            //Destroy(this);
+        }
+
+        public void CompletedQuest()
+        {
+            status = Status.Completed;
+            Debug.Log(reward);
+           // Destroy(this);
+        }
+
+        public void AcceptedQuest()
+        {
+            UI.Notifications.NotificationSystem.Instance.Post("Quest Accepted", "You've started a new quest: " + name);
+            status = Status.Active;
+        }
+
+        public void RejectedQuest()
+        {
+            status = Status.Rejected;
+            UI.Notifications.NotificationSystem.Instance.Post("Quest Rejected", "You've rejected the following quest: " + name);
+            //Destroy(this);
+        }
+    }
+}
---
+++ HexClicker/Assets/UI/Tracker/Scripts/Manager.cs
@@ -0,0 +1,56 @@
+﻿using HexClicker.Quests;
+using System;
+using System.Collections.Generic;
+using HexClicker.UI.Notifications;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace HexClicker.UI.QuestTracker
+{
+    public class Manager : MonoBehaviour
+    {
+        public static Manager Instance { get; set; }
+        public Item questItemPrefab;
+        public TextMeshProUGUI titleText;
+        [SerializeField] private int maxNumOfDisplayedQuests;
+        [SerializeField] private List<Quest> quests;
+        private Counter counter;
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+
+            counter = new Counter(quests.Count, maxNumOfDisplayedQuests);
+            titleText.text = "Number of quests: " + counter;
+        }
+
+
+        public void TestQuest(Quest quest)
+        {
+            if (quests.Count >= maxNumOfDisplayedQuests)
+            {
+                NotificationSystem.Instance.Post(title: "Too many quests", "There are too many quests active!"); 
+                return;
+            }
+            else
+            {
+                quests.Add(quest);
+                Item newQuest = Instantiate(questItemPrefab, transform);
+                newQuest.PostQuest(quest);
+            }
+        }
+
+    }
+
+   
+}
