f5748cd944c525c424c104502cc67f901e0436cd
danzabarr <danzabarr@hotmail.co.uk>
2020-03-09 18:39:30 +0000

    Easings curve example

--- HexClicker/Assets/Easings/EasingExample.cs
+++ HexClicker/Assets/Easings/EasingExample.cs
@@ -6,7 +6,7 @@ public class EasingExample : MonoBehaviour, IPointerClickHandler
 {
 
     private Coroutine routine;
-
+    public AnimationCurve curve;
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -17,8 +17,9 @@ public class EasingExample : MonoBehaviour, IPointerClickHandler
 
         //routine = StartCoroutine(Transition.AnimateSimple(1f, 1f, 1f, Transition.EaseInQuart, (float i) => transform.localScale = Vector3.one * i));
         //routine = StartCoroutine(Transition.AnimateAdvanced(1f, 1f, 1f, Transition.EaseOutBounce, (float i) => transform.localScale = Vector3.one * i));
-        routine = StartCoroutine(Transition.AnimateAdvanced(1f, 1f, 1f, Transition.EaseInOutElastic, (float i) => transform.localScale = Vector3.one * i));
+        //routine = StartCoroutine(Transition.AnimateAdvanced(1f, 1f, 1f, Transition.EaseInOutElastic, (float i) => transform.localScale = Vector3.one * i));
         //routine = StartCoroutine(Transition.AnimateAdvanced(1f, 1f, 1f, 1.70157f, Transition.EaseOutBack, (float i) => transform.localScale = Vector3.one * i));
+        routine = StartCoroutine(Transition.AnimateCurve(1f, 1f, 1f, curve, (float i) => transform.localScale = Vector3.one * i));
     }
 
 }
